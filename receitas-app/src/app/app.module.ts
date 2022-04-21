15c47b11efa7fd7bf497a826eeba6bf2cabaac89
ThiagoAnd <thiago.andrade.it@outlook.com>
2022-04-21 15:25:19 -0300

    Feat: Atualização de angular 11 para angular 12

--- receitas-app/src/app/app.module.ts
+++ receitas-app/src/app/app.module.ts
@@ -6,6 +6,11 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './template/header/header.component';
 import { FooterComponent } from './template/footer/footer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatListModule} from '@angular/material/list';
+
+
+
 
 @NgModule({
   declarations: [
@@ -16,7 +21,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
