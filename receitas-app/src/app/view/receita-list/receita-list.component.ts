c2e3c0aa657405255f1f636b142febcd6eb85fd9
ThiagoAnd <thiago.andrade.it@outlook.com>
2022-05-07 00:31:24 -0300

    Feat: Listagem de receitas

--- receitas-app/src/app/view/receita-list/receita-list.component.ts
+++ receitas-app/src/app/view/receita-list/receita-list.component.ts
@@ -2,20 +2,33 @@ import { Component, OnInit } from '@angular/core';
 import { ReceitaService } from '../receita.service';
 import { Receita } from '../../model/receita.model';
 
+
+
 @Component({
   selector: 'app-receita-list',
   templateUrl: './receita-list.component.html',
   styleUrls: ['./receita-list.component.css'],
 })
 export class ReceitaListComponent implements OnInit {
+
+
+
   pageTitle = 'Pagina de listagem';
   receitas!: Receita[];
+  dataSource!: Receita[];
+  displayedColumns: string[] = ['id', 'nome', 'tempoPreparo', 'categoria','editar','cancelar'];
+
   constructor(private service: ReceitaService) {}
   ngOnInit(): void {
     this.service.list().subscribe((receitas) => {
       this.receitas = receitas;
+      this.dataSource = this.receitas;
       console.log("Retornou as receitas")
       console.log(this.receitas)
     });
+  }
+
+  edit(id: number){
+    alert(id)
   }
 }
